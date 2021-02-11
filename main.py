9fc5ed3a7bf4d3ca3184c644a0bdd9e30aecaeb5
hochufenderyaguar <kabanova.sony@yandex.ru>
2021-02-11 17:25:52 +0300

    commit1

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from params import geocoder_params
 from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
-
 import requests
 from PIL import Image
+from math import pi, cos, sin, atan2, sqrt
+
+EARTH_RADIUS = 6372795
+
+
+def distance(x1, y1, x2, y2):
+    lat1 = x1 * pi / 180
+    lat2 = x2 * pi / 180
+    long1 = y1 * pi / 180
+    long2 = y2 * pi / 180
+
+    cl1 = cos(lat1)
+    cl2 = cos(lat2)
+    sl1 = sin(lat1)
+    sl2 = sin(lat2)
+    delta = long2 - long1
+    cdelta = cos(delta)
+    sdelta = sin(delta)
+
+    y = sqrt(pow(cl2 * sdelta, 2) + pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2))
+    x = sl1 * sl2 + cl1 * cl2 * cdelta
+
+    ad = atan2(y, x)
+    dist = ad * EARTH_RADIUS
+
+    return dist
+
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 response = requests.get(geocoder_api_server, params=geocoder_params)
@@ -22,14 +47,40 @@ toponym_coodrinates = toponym["Point"]["pos"]
 # Долгота и широта:
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-delta = "0.005"
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-# Собираем параметры для запроса к StaticMapsAPI:
-map_params = {
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join(['0.008211', '0.004623']),
+    "type": "biz"
+}
+
+response = requests.get(search_api_server, params=search_params)
+
+if not response:
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+
+# Получаем первую найденную организацию.
+organization = json_response["features"][0]
+# Название организации.
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+# Адрес организации.
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+org_time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+
+# Получаем координаты ответа.
+point = organization["geometry"]["coordinates"]
+org_point = "{0},{1}".format(point[0], point[1])
+
+map_params = {
     "l": "map",
-    'pt': ",".join(toponym_coodrinates.split(" ")) + ',round'
+    'pt': ",".join(toponym_coodrinates.split(" ")) + ',round~' + "{0},pm2dgl".format(org_point)
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
@@ -40,3 +91,8 @@ Image.open(BytesIO(
     response.content)).show()
 # Создадим картинку
 # и тут же ее покажем встроенным просмотрщиком операционной системы
+
+print(org_name)
+print(org_address)
+print(org_time)
+print(distance(point[0], point[1], float(toponym_lattitude), float(toponym_longitude)), 'метров')
